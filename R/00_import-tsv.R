0a68934bb8ea33048ff21890081942ce6213a708
Alex Barth <abarth1014@gmail.com>
2024-07-23 18:24:49 -0500

    create project

---
+++ R/00_import-tsv.R
@@ -0,0 +1,22 @@
+####
+# Formatting Ecotaxa Data #####
+####
+
+
+rm(list = ls()) # this wipes your environment
+library(EcotaxaTools) # this is my package
+
+raw <- read_etx('./data/raw.tsv')
+
+
+### Need to format to merge replicates
+
+
+## creat a sample ID
+
+
+## merge with salinity information
+
+
+
+### calc par size spectra
