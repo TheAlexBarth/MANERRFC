f80aac648d53450834d84a1996716eb3c366b91c
Alex Barth <abarth1014@gmail.com>
2025-03-05 14:16:19 -0600

    add pdsi plots

---
+++ R/ex-pdsi-plots.R
@@ -0,0 +1,43 @@
+#####
+# Example comparison wet dry
+#######
+
+rm(list = ls())
+library(lubridate)
+
+# do diatoms change in wet vs dry periods?
+
+etx <- readRDS('./data/02-full_merged.rds')
+env <- readRDS('./data/01-environ_clean.RDS')
+
+
+env$PDSI$yearmo <- as.Date(env$PDSI$Date)
+plank_pdsi <- etx$conc |> 
+  filter(
+    taxa %in% etx$names$diatom
+  ) |> 
+  group_by(
+    sample_site, yearmo
+  ) |> 
+  summarize(
+    mean_c = sum(pgC_L),
+    sd_c = sd(pgC_L)
+  ) |> 
+  left_join(
+    env$PDSI |> 
+      select(
+        yearmo,
+        pdsi
+      )
+  )
+
+
+ggplot() +
+  geom_point(
+    data = plank_pdsi,
+    aes(
+      x = pdsi,
+      y = mean_c,
+      color = sample_site
+    )
+  )
