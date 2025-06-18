a37dbe65a70cbd571ffa150e0224f74758d0b4cf
Alex Barth <abarth1014@gmail.com>
2025-06-18 17:18:21 -0500

    delete david messy output

--- R/03-gen_taxa_pred-mod.R
+++ R/03-gen_taxa_pred-mod.R
@@ -1,5 +1,5 @@
 rm(list= ls())
-library(rstan)
+library(cmdstanr)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
@@ -10,146 +10,192 @@ source('./R/utils-mod_making.R')
 etx <- readRDS("./data/02-full_merged.RDS")
 
 taxa_group <- 'mz'
-niter = 8000
-nburn = 1000
-nchains = 3
+log_file <- './logs/20250617-mz_run.log'
+file.create(log_file)
 
 
-core_data <- prep_component_data(taxa_group)
+
 
 ###################
 # MARK: MODEL CONSTRUCTION 
 ###################
 
-# region \- make predictors ------------
-counts_poss_preds <- c('sal','t','do','turb','P','NH4','N','windspeed')
-indv_poss_preds <- c('sal','t','P','NH4','N')
 
-
-# region \- possible preds --------------------------------
-
-poss_full <- list(
-  full = list(
-    count_preds = counts_poss_preds,
-    indv_preds = indv_poss_preds
-  ),
-  alt = list(
-    count_preds = c('sal','t','windspeed','P','NH4','N'),
-    indv_preds  = indv_poss_preds
-  ),
-  env_only = list(
-    count_preds = c('sal','t','do','turb', 'windspeed'),
-    indv_preds = c('sal','t','P','NH4','N')
-  ),
-  red1 = list(
-    count_preds = c('sal','t','turb', 'windspeed'),
-    indv_preds = c('sal','t','P','NH4','N')
-  ),
-  red2 = list(
-    count_preds = c('sal','t','windspeed'),
-    indv_preds = c('sal','t','P','NH4','N')
-  ),
-  red3 = list(
-    count_preds = c('sal','windspeed'),
-    indv_preds = c('sal','t','P','NH4','N')
-  ),
-  red4 = list(
-    count_preds = c('sal'),
-    indv_preds = c('sal','t','P','NH4','N')
-  )
-)
-poss_matrix <- list()
-for(name in names(poss_full)) {
-  poss_matrix[[name]] <- prep_pred_matrix(
-    poss_full[[name]]$count_preds,
-    poss_full[[name]]$indv_preds,
-    core_data
-  )
-}
-
-no_indv_preds <- lapply(
-  names(poss_matrix),
-  function(x) 
-    return(list(
-      X_counts = poss_matrix[[x]]$X_counts,
-      X_indv = matrix(1, nrow(core_data$indv_mes), 1),
-      pred_indv = matrix(1, nrow(core_data$all_conc), 1) # take this matrix as indv if noindv is selected
-    ))
-)
-names(no_indv_preds) <- paste0(names(poss_matrix), '_noindv')
 
 # region \-\- all together -------------
-all_preds <- c(poss_matrix, no_indv_preds)
-
  
-main_model <- stan_model(file = './stan/gen-hierachical_mixture_model.stan')
+main_model <- cmdstan_model('./stan/gen-hierachical_mixture_model.stan')
 
-mod_list <- list()
-pb = txtProgressBar(min = 0, max = length(names(all_preds)))
-for(name in names(all_preds)) {
-
-  progress = which(names(all_preds) == name)
-  mod_list[[name]] <- fit_component_mod(
-    all_preds[[name]],
-    core_data,
-    main_model,
-    chains = nchains,
-    iter = niter,
-    warmup = nburn,
-    cores = parallel::detectCores()
-  )
-  cat(
-    "\n\n\n",'---------------------------','\n',
-    "Finished Model:", name, 'at', as.character(Sys.time()),
-    '\n', "---------------------------",'\n\n\n'
-  )
-  setTxtProgressBar(pb, progress)
-}
-
-
-########
-# MARK: Diagnositcs
-########
-
-
-all_rmspe <- sapply(
-  names(mod_list),
-  function(x) mod_list[[x]] |> rstan::extract('RMSPE')
+bmass_priors <- list(
+  b_mu = 8,
+  b_sig = 3,
+  tau_mu = 1,
+  tau_sig = 1.5
 )
 
-rmspe_summary <- sapply(all_rmspe, quantile, probs = c(0.025,0.5,0.975))
-write.csv(rmspe_summary,paste0('./output/03-',taxa_group,'-rmspe.csv'))
 
-plot_df <- sapply(
-  all_rmspe,
-  function(x) x[which(x < quantile(x, 0.975) & x > quantile(x, 0.025))]
-) |> 
-  as.data.frame()
+# region \- make predictors ------------
+counts_poss_preds <- c('sal','t','do','turb','CHLA_N','windspeed', "TotalPAR", 'P','NH4','N')
 
-min_name <- all_rmspe |> 
-  sapply(mean) |> 
-  which.min() |> 
-  names()
+cat(
+  '\n','Launching Model Selection','\n',
+  sep = "\n", file = log_file,
+  append = TRUE
+)
+full_data <- prep_component_data(taxa_group, counts_poss_preds)
+full_mod <- fit_mod(
+  data_list = full_data,
+  count_preds = counts_poss_preds,
+  bmass_priors,
+  main_model,
+  iter = 7000,
+  chains = 10,
+  parallel_chains = 10,
+  iter_warmup = 2000,
+  thin = 5,
+  refresh = 0
+)
+# make sure to check before running that the full model passes post-pred check
+cur_pred = counts_poss_preds
+cur_mod = full_mod
+cur_rmspe <- cur_mod$draws('RMSPE',,'df')$RMSPE
+improve = TRUE
 
-ggplot(plot_df |> 
-  pivot_longer(
-    cols = everything(),names_to = 'Model',values_to = 'RMSPE'
-  )) +
-  geom_violin(
-    aes(
-      x = Model,
-      y = RMSPE
+while(improve & length(cur_pred)>1) {
+  improve <- FALSE
+  cat(
+    "",paste0('Current Pars: ', paste0(cur_pred, collapse = ', ')),
+    sep = "\n", file = log_file,
+    append = TRUE
+  )
+  for(i in rev(seq_along(cur_pred))) {
+    drop_pred <- cur_pred[i]
+
+    cat(
+      "",'=================',paste0('Checking: ', drop_pred), '=================', 
+      sep = "\n", file = log_file,
+      append = TRUE
     )
+
+    can_data <- prep_component_data(taxa_group, cur_pred[-i])
+    can_mod <- fit_mod(
+      data_list = can_data,
+      count_preds = cur_pred[-i],
+      bmass_priors,
+      main_model,
+      iter = 4000,
+      chains = 5,
+      parallel_chains = 5,
+      iter_warmup = 2000,
+      thin = 2,
+      refresh = 0
+    )
+    # region \-\- check bayes p value ----------
+    pdf <- can_mod$draws(c('MSE_obs','MSE_til'), format = 'df')
+    p <- mean(pdf$MSE_obs > pdf$MSE_til)
+    if(p > 0.95 | p < 0.05) {
+      p_accept <- FALSE
+    } else {
+      p_accept <- TRUE
+    }
+    # endregion
+
+    # region \-\- check RMSE ------------
+    can_rmspe <- can_mod$draws('RMSPE',,'df')$RMSPE
+    p_better <- mean(can_rmspe <= cur_rmspe)
+    # endregion
+
+    # region \-\- check effect -------------
+    # note this checks the currrent model not candidate!
+    betas <- cur_mod$draws('beta_count', format = 'df')
+    no_effect <- betas |> 
+      select(grep(paste0(i+1,"]$"),names(betas))) |> 
+      apply(2, quantile, probs = c(0.025,0.975)) |> 
+      overlaps_zero() |> 
+      all()
+    # endregion
+
+    if(p_better > 0.40 & p_accept & no_effect) {
+      improve <- TRUE
+      cur_mod <- can_mod
+      cur_pred <- cur_pred[-i]
+      cur_rmspe <- can_rmspe
+      cat(
+        '====================',
+        paste0('Dropping: ', drop_pred), '====================',
+        sep = "\n", file = log_file,
+        append = TRUE
+      )
+      break
+    } else {
+      # log message:
+      if(p_better <= 0.40) {
+        cat(
+          '====================',
+          paste0('Keeping: ', drop_pred, ' Worsened RMPSE beyond threshold'), '====================',
+          sep = "\n", file = log_file,
+          append = TRUE
+        )
+      }
+      if(!p_accept) {
+        cat(
+          '====================',
+          paste0('Keeping: ', drop_pred, ' PPC fail'), '====================',
+          sep = "\n", file = log_file,
+          append = TRUE
+        )
+      }
+      if(!no_effect) {
+        cat(
+          '====================',
+          paste0('Keeping: ', drop_pred, ' Effect at Some Level'), '====================',
+          sep = "\n", file = log_file,
+          append = TRUE
+        )
+      }
+    }
+  }
+}
+cat(
+  '====================',
+  paste0('Model Finalized: ', paste0(cur_pred, collapse = ', ')),
+  '====================',
+  sep = "\n", file = log_file,
+  append = TRUE
+)
+
+# manual tests:
+
+modterms_1 = c("sal", "t", "do", "P", "NH4")
+
+modterms_2 = c("sal", "t", "do", "P", "NH4")
+
+
+# # consider a manual check for dropping all nutrient parameters
+# #
+# # manual check for no nutrient params:
+# can_preds <- c('sal','t','do','turb','windspeed')
+# can_data <- prep_component_data(taxa_group, can_preds)
+# can_mod <- fit_mod(
+#   data_list = can_data,
+#   count_preds = can_preds,
+#   bmass_priors,
+#   main_model,
+#   iter = 4000,
+#   chains = 5,
+#   parallel_chains = 5,
+#   iter_warmup = 2000,
+#   thin = 2,
+#   refresh = 0
+# )
+# did not improve results...
+
+ggplot() +
+  geom_density(
+    aes(x = can_rmspe),
+    fill = 'blue', alpha = 0.25
   ) +
-  geom_point(
-    aes(
-      x = Model,
-      y = mean(RMSPE)
-    )
-  )+
-  labs(subtitle = paste0('Best Model is ',min_name))+
-  theme_classic()+
-  theme(axis.text = element_text(angle = 45))
-
-
-ggsave(paste0('./output/03-best_fit-', taxa_group,'.png'))
+  geom_density(
+    aes(x = cur_rmspe),
+    fill = 'red', alpha = 0.25
+  )
